21b218c06d5d49654641170e1cef303c63d583fe
Kevin Janvier <kevin.janvier5@gmail.com>
2018-03-09 16:39:11 +0300

    add constrain

--- app/src/main/java/com/thinkdevs/leanui/MainActivity.kt
+++ app/src/main/java/com/thinkdevs/leanui/MainActivity.kt
@@ -2,11 +2,25 @@ package com.thinkdevs.leanui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		
+		object : CountDownTimer(1000, 1000){
+			override fun onFinish() {
+			
+				counter.text = "Left : 0"
+			}
+			
+			override fun onTick(p0: Long) {
+				counter.text = "Left : ${p0 / 1000}"
+			}
+			
+		}
 	}
 }
